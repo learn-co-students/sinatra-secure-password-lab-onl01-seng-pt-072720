b09b4baf073de7a9abff2371ba0ab958cc01d7ae
Christie Yoon <cleex1999@gmail.com>
2020-10-30 17:08:05 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,18 +17,17 @@ class ApplicationController < Sinatra::Base
     erb :signup
   end
 
-  post "/signup" do
-    #your code here
-      if params[:username] != "" && params[:password] != ""
-          @user = User.create(username: params[:username], password: params[:password])
-		       if @user && @user.authenticate(params[:password])
-              session[:user_id] = @user.id
-	 		        redirect to '/login'
-           end
+
+    post "/signup" do
+      if params[:username] == "" || params[:password] == ""
+        redirect '/failure'
       else
-	 		    redirect to '/failure'
- 	    end
-  end
+        User.create(username: params[:username], password: params[:password])
+        redirect '/login'
+      end
+
+    end
+
 
 
   get '/account' do
@@ -42,14 +41,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
-    post "/signup" do
-     if params[:username] == "" || params[:password] == ""
-       redirect '/failure'
-     else
-       User.create(username: params[:username], password: params[:password])
-       redirect '/login'
-     end
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/account"
+    else
+      redirect to "/failure"
+    end
   end
 
   get "/failure" do
